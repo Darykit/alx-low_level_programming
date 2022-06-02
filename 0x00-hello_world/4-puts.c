700607b866f56f245bb9b79b098836e9f9ea106b
Damilare <lawaldamilare0404@gmail.com>
2022-06-02 15:14:26 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
 
-int main()
