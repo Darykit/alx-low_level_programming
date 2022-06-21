f13700ec7671b81e83095ed95824d9de8d6d9d17
Damilare <lawaldamilare0404@gmail.com>
2022-06-21 01:02:08 -0700

    1memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: character
+ * @src: character
+ * @n: unsigned int
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
